faadbb05da9ab8c23a4fe52ab89829b23c0e5928
GiDuk-Kim <137968025+GiDuk-Kim@users.noreply.github.com>
2023-07-30 07:04:27 +0900

    웹서비주소 전역상수로 지정 v0.1
    Request 파일에서 외부 웹 서버 주소를
    각각 지정한 것을 Constants class 를
    만들고 것을 각 iimportt 해서 사용하게
    수정

--- app/src/main/java/com/himedia/androidshoppingmall/Data/Constants.java
+++ app/src/main/java/com/himedia/androidshoppingmall/Data/Constants.java
@@ -1,6 +1,6 @@
 package com.himedia.androidshoppingmall.Data;
 
 public class Constants {
-    public static final String WEB_URL = "http://3.37.214.236/android_001/";   // aws server
-//  private static final String URL ="http://gdkgate.dothome.co.kr/android_001/";    //test server
+    public static final String WEB_URL = "http://3.37.214.236/android_001/";         // aws server
+//  private static final String URL ="http://gdkgate.dothome.co.kr/android_001/";    // test server
 }
